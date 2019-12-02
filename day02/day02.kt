756e2a78e6af8e96747d8cd98fe3a579a9ac9b32
Jonathan Harlap <jharlap@gmail.com>
2019-12-02 00:38:46 -0500

    Update day02.kt

--- day02/day02.kt
+++ day02/day02.kt
@@ -1,10 +1,28 @@
 fun main() {
     val input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,1,13,23,27,1,27,6,31,2,31,6,35,2,6,35,39,1,39,5,43,1,13,43,47,1,6,47,51,2,13,51,55,1,10,55,59,1,59,5,63,1,10,63,67,1,67,5,71,1,71,10,75,1,9,75,79,2,13,79,83,1,9,83,87,2,87,13,91,1,10,91,95,1,95,9,99,1,13,99,103,2,103,13,107,1,107,10,111,2,10,111,115,1,115,9,119,2,119,6,123,1,5,123,127,1,5,127,131,1,10,131,135,1,135,6,139,1,10,139,143,1,143,6,147,2,147,13,151,1,5,151,155,1,155,5,159,1,159,2,163,1,163,9,0,99,2,14,0,0"
     //val input = "1,1,1,4,99,5,6,0,99"
+    
+    // part 1
+    val res = runProgram(input, 12, 2)
+    println("HALT! Pos 0 = $res")
+    
+    // part 2
+    val exp = 19690720
+    for (noun in 0..99) {
+        for (verb in 0..99) {
+            if (runProgram(input, noun, verb) == exp) {
+                println("FOUND! noun = $noun verb = $verb")
+                return
+            }
+        }
+    }
+}
+
+fun runProgram(input: String, noun: Int, verb: Int): Int {
     val state: MutableList<Int> = input.split(",").map {it.toInt()}.toMutableList()
     
-    state[1] = 12
-    state[2] = 2
+    state[1] = noun
+    state[2] = verb
     
     var idx: Int = 0
     while (state[idx] != 99) {
@@ -19,6 +37,5 @@ fun main() {
         }
         idx += 4;
     }
-    println("HALT! Pos 0 = ${state[0]}")
-    println(state)
+    return state[0]
 }
